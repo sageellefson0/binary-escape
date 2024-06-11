82e2bc0942881b1d31c47d3b0f6f23bbcb6970f8
Sage Ellefson <sagecole0@gmail.com>
2024-06-11 15:29:27 -0500

    Character animation is working - limited to a box. Need custom sprite sheet and better limitations for the character

--- script.js
+++ script.js
@@ -1,67 +1,78 @@
-document.addEventListener('DOMContentLoaded', () => {
-    var character1 = document.getElementById("character1");
+var character = document.querySelector(".character");
 
-    //start in the middle of the screen
-    var x = window.innerWidth / 2;
-    var y = window.innerHeight / 2;
-    var held_directions = []; //State of which arrow keys we are holding down
-    var speed = 5; //How fast the character moves in pixels per frame
+var x = 90;
+var y = 34;
+var held_directions = []; // Array for directions of character
+var speed = 1.0; // Speed in pixels of character
 
-    const placeCharacter = () => {
-        const held_direction = held_directions[0];
-        if (held_direction) {
-            if (held_direction === directions.right) { x += speed; }
-            if (held_direction === directions.left) { x -= speed; }
-            if (held_direction === directions.down) { y += speed; }
-            if (held_direction === directions.up) { y -= speed; }
-            character1.setAttribute("facing", held_direction);
-        }
-        character1.setAttribute("walking", held_direction ? "true" : "false");
+const placeCharacter = () => {
+   
+   var pixelSize = parseInt(
+      getComputedStyle(document.documentElement).getPropertyValue('--pixel-size')
+   );
+   
+   const held_direction = held_directions[0];
+   if (held_direction) {
+      if (held_direction === directions.right) {x += speed;}
+      if (held_direction === directions.left) {x -= speed;}
+      if (held_direction === directions.down) {y += speed;}
+      if (held_direction === directions.up) {y -= speed;}
+      character.setAttribute("facing", held_direction);
+   }
+   character.setAttribute("walking", held_direction ? "true" : "false");
+   
+   // Walls around character
+   var leftLimit = -8;
+   var rightLimit = (16 * 11)+8;
+   var topLimit = -8 + 32;
+   var bottomLimit = (16 * 7);
+   if (x < leftLimit) { x = leftLimit; }
+   if (x > rightLimit) { x = rightLimit; }
+   if (y < topLimit) { y = topLimit; }
+   if (y > bottomLimit) { y = bottomLimit; }
+   
 
-        // Limits to keep the character within the viewport
-        var leftLimit = 0;
-        var rightLimit = window.innerWidth - character1.offsetWidth;
-        var topLimit = 0;
-        var bottomLimit = window.innerHeight - character1.offsetHeight;
+   character.style.transform = `translate3d( ${x*pixelSize}px, ${y*pixelSize}px, 0 )`;  
+}
 
-        if (x < leftLimit) { x = leftLimit; }
-        if (x > rightLimit) { x = rightLimit; }
-        if (y < topLimit) { y = topLimit; }
-        if (y > bottomLimit) { y = bottomLimit; }
 
-        character1.style.transform = `translate3d(${x}px, ${y}px, 0)`;
-    }
+// Game loop
+const step = () => {
+   placeCharacter();
+   window.requestAnimationFrame(() => {
+      step();
+   })
+}
+step();
 
-    const step = () => {
-        placeCharacter();
-        window.requestAnimationFrame(step);
-    }
-    step();
 
-    const directions = {
-        up: "up",
-        down: "down",
-        left: "left",
-        right: "right",
-    }
-    const keys = {
-        38: directions.up,
-        37: directions.left,
-        39: directions.right,
-        40: directions.down,
-    }
-    document.addEventListener("keydown", (e) => {
-        var dir = keys[e.which];
-        if (dir && held_directions.indexOf(dir) === -1) {
-            held_directions.unshift(dir);
-        }
-    })
 
-    document.addEventListener("keyup", (e) => {
-        var dir = keys[e.which];
-        var index = held_directions.indexOf(dir);
-        if (index > -1) {
-            held_directions.splice(index, 1);
-        }
-    });
+/* Direction key state - shorthand for future use*/
+const directions = {
+   up: "up",
+   down: "down",
+   left: "left",
+   right: "right",
+}
+const keys = {
+   38: directions.up,
+   37: directions.left,
+   39: directions.right,
+   40: directions.down,
+}
+document.addEventListener("keydown", (e) => {
+   var dir = keys[e.which];
+   if (dir && held_directions.indexOf(dir) === -1) {
+      held_directions.unshift(dir)
+   }
+})
+
+document.addEventListener("keyup", (e) => {
+   var dir = keys[e.which];
+   var index = held_directions.indexOf(dir);
+   if (index > -1) {
+      held_directions.splice(index, 1)
+   }
 });
+
+
