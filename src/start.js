c6eac5835301b45648146ba2e43a898315e5e081
prlakshm <pranavi_lakshminarayanan@brown.edu>
2024-08-01 11:44:22 -0400

    Feat: pushed with girl-intern animation

---
+++ src/start.js
@@ -0,0 +1,22 @@
+/* ----------------- Sprite Character Javascript --------------------- */
+
+// const maleCharacterImg = document.getElementById("male-intern");
+// const femaleCharacterImg = document.getElementById("female-intern");
+//         const imageSources = [
+//             "./images/malecharacter1.png",
+//             "./images/malecharacter2.png"
+//         ];
+
+//         let currentImageIndex = 0;
+
+//         const switchImage = () => {
+//             currentImageIndex = (currentImageIndex + 1) % imageSources.length;
+//             maleCharacterImg.src = imageSources[currentImageIndex];
+//             femaleCharacterImg.src = imageSources[currentImageIndex];
+//         };
+
+//         // Set an interval to switch images every 500 milliseconds
+//         setInterval(switchImage, 500);
+
+
+/* ----------------- 3D Character Three.js --------------------- */
