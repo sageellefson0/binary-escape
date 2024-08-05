9f7a5923a3deb86f8d61f5c573baccad61fd7fa4
prlakshm <pranavi_lakshminarayanan@brown.edu>
2024-08-05 10:43:51 -0400

    Feat: fade-in for end page working

---
+++ src/end.js
@@ -0,0 +1,10 @@
+// Wait for the DOM content to load
+document.addEventListener("DOMContentLoaded", function() {
+    // Select the video element
+    const video = document.getElementById('female-intern-dancing');
+
+    // Set a timeout to play the video after 3 seconds
+    setTimeout(function() {
+        video.play();
+    }, 2500);
+});
