54d086c716f305e73840d83bd0e4b6c483b33c3d
prlakshm <pranavi_lakshminarayanan@brown.edu>
2024-08-05 12:13:52 -0400

    Feat: added fade out end.html

--- src/end.js
+++ src/end.js
@@ -8,3 +8,17 @@ document.addEventListener("DOMContentLoaded", function() {
         video.play();
     }, 2500);
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const endPage = document.querySelector('.end-page');
+
+    // Initial fade-in animation
+    setTimeout(() => {
+        endPage.style.opacity = '1'; // Ensure the fade-in animation starts
+    }, 0);
+
+    // Trigger fade-out animation after 15 seconds
+    setTimeout(() => {
+        endPage.style.animation = 'fadeOutOpacity 3s ease-in-out forwards';
+    }, 16000); // 15 seconds
+});
