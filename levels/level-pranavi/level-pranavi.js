c3e3659e13ec2f3b9ac77b2898c30a314f2830cc
Sage Ellefson <sagecole0@gmail.com>
2024-08-24 18:25:47 -0500

    Feat: Adjusted Pranavi's level completion logic for desktop screen

--- levels/level-pranavi/level-pranavi.js
+++ levels/level-pranavi/level-pranavi.js
@@ -213,8 +213,10 @@ document.addEventListener("DOMContentLoaded", () => {
     dialogButton.addEventListener('click', function() {
         if (passwordCorrect) {
             completeLevel('word');
-            window.location.href = "../desktop/level-desktop.html";
-             // Replace with actual link to next round
+            // Timeout provides buffer for completeLevel to ber logged correctly - don't remove
+            setTimeout(function() {
+              window.location.href = "../desktop/level-desktop.html";
+          }, 2000);
         } else {
             hideDialog();
         }
